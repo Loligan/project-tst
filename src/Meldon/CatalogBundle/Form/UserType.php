00afb7044fca759cf0987dbe592ce3150f8959c8
Vitctor-L <tfo2011@gmail.com>
2017-05-18 22:27:39 +0300

    123

---
+++ src/Meldon/CatalogBundle/Form/UserType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Meldon\CatalogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('username')->add('password')->add('email')->add('isActive');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Meldon\CatalogBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'meldon_catalogbundle_user';
+    }
+
+
+}
